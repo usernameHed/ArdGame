7e84cffa0af6f48a7c33d06d31ef4c3a33a877dc
DESKTOP-20HJOKH\Hed <ugo@belfiore.fr>
2018-09-24 16:39:28 +0200

    FIRST PUSH

---
+++ ArdGame/Assets/_Scripts/Game/ArdManager.cs
@@ -0,0 +1,11 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+[TypeInfoBox("[ILevelLocal] Manage Setup Scene behaviour")]
+public class ArdManager : MonoBehaviour, ILevelLocal
+{
+    public void InitScene()
+    {
+        Debug.Log("INIT ArdManager !!");
+    }
+}
