5b85f6120aa30ba66e318d637e90734d2dd427bf
DESKTOP-20HJOKH\Hed <ugo@belfiore.fr>
2018-09-25 16:01:00 +0200

    Polish x10

--- ArdGame/Assets/_Scripts/Game/ArdManager.cs
+++ ArdGame/Assets/_Scripts/Game/ArdManager.cs
@@ -1,20 +1,55 @@
 ﻿using Sirenix.OdinInspector;
 using UnityEngine;
+using TMPro;
 
 [TypeInfoBox("[ILevelLocal] Manage Setup Scene behaviour")]
 public class ArdManager : MonoBehaviour, ILevelLocal
 {
     [SerializeField]
+    private float timeAfterWinningForCamera = 1.5f;
+    [SerializeField]
     private float timeAfterWinning = 2f;
+    [SerializeField]
+    private string levelText = "Level 1";
+
+    [SerializeField]
+    private TextMeshProUGUI text;
+    [SerializeField]
+    private CameraMove camMove;
+
+    [SerializeField]
+    private Animator animLevel;
 
     private void OnEnable()
     {
         EventManager.StartListening(GameData.Event.GameOver, GameOver);
+        EventManager.StartListening(GameData.Event.PlayerMove, PlayerMove);
     }
 
     public void InitScene()
     {
         Debug.Log("INIT ArdManager !!");
+        text.text = levelText;
+        camMove.enabled = false;
+    }
+
+    private void PlayerMove()
+    {
+        animLevel.Play("Out");
+    }
+
+    private void InputLevel()
+    {
+        //go to first level !
+        if (Input.GetButton("Restart"))
+        {
+            GameManager.Instance.SceneManagerLocal.PlayPrevious();
+        }
+        //quit application !
+        if (Input.GetButton("Cancel"))
+        {
+            GameManager.Instance.SceneManagerLocal.Quit();
+        }
     }
 
     /// <summary>
@@ -22,12 +57,23 @@ public class ArdManager : MonoBehaviour, ILevelLocal
     /// </summary>
     private void GameOver()
     {
+        Invoke("ActiveCamera", timeAfterWinningForCamera);
         Invoke("NextLevel", timeAfterWinning);
+    }
+
+    private void ActiveCamera()
+    {
+        camMove.enabled = true;
     }
 
     private void NextLevel()
     {
         GameManager.Instance.SceneManagerLocal.PlayNext();
+    }
+
+    private void Update()
+    {
+        InputLevel();
     }
 
     private void OnDisable()
