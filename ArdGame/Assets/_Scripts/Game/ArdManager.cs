17d321a6c194740178035fc9354d4963ba00c562
DESKTOP-20HJOKH\Hed <ugo@belfiore.fr>
2018-09-25 13:59:01 +0200

    NextLevel Done

--- ArdGame/Assets/_Scripts/Game/ArdManager.cs
+++ ArdGame/Assets/_Scripts/Game/ArdManager.cs
@@ -4,8 +4,34 @@ using UnityEngine;
 [TypeInfoBox("[ILevelLocal] Manage Setup Scene behaviour")]
 public class ArdManager : MonoBehaviour, ILevelLocal
 {
+    [SerializeField]
+    private float timeAfterWinning = 2f;
+
+    private void OnEnable()
+    {
+        EventManager.StartListening(GameData.Event.GameOver, GameOver);
+    }
+
     public void InitScene()
     {
         Debug.Log("INIT ArdManager !!");
+    }
+
+    /// <summary>
+    /// called when winning, after X seconde, go to next level !
+    /// </summary>
+    private void GameOver()
+    {
+        Invoke("NextLevel", timeAfterWinning);
+    }
+
+    private void NextLevel()
+    {
+        GameManager.Instance.SceneManagerLocal.PlayNext();
+    }
+
+    private void OnDisable()
+    {
+        EventManager.StopListening(GameData.Event.GameOver, GameOver);
     }
 }
---
+++ ArdGame/Assets/_Scripts/Game/NextLevel.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class NextLevel : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject collidersWall;       //link to walls
+
+    private bool enabledScript = true;      //tell if this script should be active or not
+
+
+    private void OnEnable()
+    {
+        EventManager.StartListening(GameData.Event.GameOver, GameOver);
+    }
+
+    private void Start()
+    {
+        collidersWall.SetActive(false);             //desactive wall at start !
+        enabledScript = true;               //active this script at start
+    }
+
+    /// <summary>
+    /// called by OnTriggerEnter, when player enter in the hole
+    /// </summary>
+    private void GameOver()
+    {
+        enabledScript = false;
+        collidersWall.SetActive(true);              //active the wall (for blocking player)
+    }
+
+    /// <summary>
+    /// trigger the player, and trigger the event GameOver !
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!enabledScript)
+            return;
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            EventManager.TriggerEvent(GameData.Event.GameOver);
+        }
+    }
+
+    private void OnDisable()
+    {
+        EventManager.StopListening(GameData.Event.GameOver, GameOver);
+    }
+}
