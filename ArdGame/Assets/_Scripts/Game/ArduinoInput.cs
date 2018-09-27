5ded88d442d07324ca8f7b16040491fe73ddbc10
DESKTOP-20HJOKH\Hed <ugo@belfiore.fr>
2018-09-27 19:01:44 +0200

    final ?

---
+++ ArdGame/Assets/_Scripts/Game/ArduinoInput.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class ArduinoInput : SingletonMono<ArduinoInput>
+{
+	private AndroidJavaObject plugin;
+
+	void Start()
+	{
+        Init();
+    }
+
+    /// <summary>
+    /// init arduino
+    /// </summary>
+    private void Init()
+    {
+        plugin = new AndroidJavaObject("com.project.unityotglib.OtgReader");
+        AndroidJavaClass playerClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject activity = playerClass.GetStatic<AndroidJavaObject>("currentActivity");
+        plugin.CallStatic("Init", new object[1] { activity });
+    }
+
+	void Update ()
+	{
+		try
+		{
+			plugin.Call("onFrameUpdate");
+            ArdManager.Instance.InputLevel(plugin.Call<string>("getValue"));
+			//text.text = "Value: " + plugin.Call<string>("getValue");
+		}
+		catch(System.Exception e)
+		{
+
+		}
+	}
+}
