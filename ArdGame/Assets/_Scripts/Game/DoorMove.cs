7f8fd4024aee45de7fbe5f33001aa22ce464ecfb
DESKTOP-20HJOKH\Hed <ugo@belfiore.fr>
2018-09-27 11:09:34 +0200

    trail modif

--- ArdGame/Assets/_Scripts/Game/DoorMove.cs
+++ ArdGame/Assets/_Scripts/Game/DoorMove.cs
@@ -13,6 +13,9 @@ public class DoorMove : Door
 
     private bool isOpenning = false;
 
+    /// <summary>
+    /// open or close (set trigger of animation)
+    /// </summary>
     public override void Move(bool input)
     {
         if (input && isOpen == false && isOpenning == false && isClosed)
@@ -30,6 +33,9 @@ public class DoorMove : Door
         }
     }
 
+    /// <summary>
+    /// called when animation Open is finished (called by animator)
+    /// </summary>
     public void OpenFinish()
     {
         isOpenning = false;
@@ -37,11 +43,17 @@ public class DoorMove : Door
         isClosed = false;
     }
 
+    /// <summary>
+    /// called when animation Close is finished (called by animator)
+    /// </summary>
     public void CloseFinish()
     {
         isClosed = true;
     }
 
+    /// <summary>
+    /// define if player is inside or not player (has to be with tag Player)
+    /// </summary>
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
@@ -50,6 +62,9 @@ public class DoorMove : Door
         }
     }
 
+    /// <summary>
+    /// define if player is inside or not player (has to be with tag Player)
+    /// </summary>
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
